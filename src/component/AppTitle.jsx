52fab1a84d301e622c83d7efa3405499a5c026cf
rarsidha <rarsidha@gmail.com>
2024-05-28 12:29:55 +0530

    fixed config mispell,add format script to package.json

--- src/component/AppTitle.jsx
+++ src/component/AppTitle.jsx
@@ -1,14 +1,12 @@
-
-
-export default function AppTitle(props){
-    const{ title="Box-Office",
-    subtitle="Are you looking for movie or actor?"}
-    =props;
-    return ( 
+export default function AppTitle(props) {
+  const {
+    title = 'Box-Office',
+    subtitle = 'Are you looking for movie or actor?',
+  } = props;
+  return (
     <div>
-    <h1>{title}</h1>
-    <p>{subtitle}</p>   
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
     </div>
-   );
-   }
-   
+  );
+}
--- src/component/MainLayout.jsx
+++ src/component/MainLayout.jsx
@@ -1,15 +1,15 @@
-
-import {Outlet} from 'react-router-dom';
-import Navs from "./Navs";
+import { Outlet } from 'react-router-dom';
+import Navs from './Navs';
 import AppTitle from './AppTitle';
 
-const MainLayout=()=>{
- return ( <div>
-        <AppTitle/>
-        <Navs />
-        <Outlet/>
+const MainLayout = () => {
+  return (
+    <div>
+      <AppTitle />
+      <Navs />
+      <Outlet />
     </div>
-);
+  );
 };
 
 export default MainLayout;
